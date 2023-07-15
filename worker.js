e03d3fc16fa083a7eb12de10f2bbbba33dcbc8b0
Miguel Gutierrez <gt2rz.dev@gmail.com>
2023-07-15 10:10:08 -0500

    initial commit

---
+++ worker.js
@@ -0,0 +1,38 @@
+function csvProcess({ content, separator = "," }) {
+  const lines = content.split("\n");
+  let errors = [];
+
+  self.postMessage({"type": "init", "lines": lines.length });
+
+  for (var i = 0; i < lines.length; i++) {
+    if (i === 0) {
+      continue;
+    }
+
+    const line = lines[i].split(separator);
+    validateLine(line, i);      
+    
+    self.postMessage({"type": "progress", "linesProcessed": i, "linesErrors": errors.length });
+
+  }
+
+  self.postMessage({"type": "done", "linesProcessed": i, "linesErrors": errors.length, "errors": errors });
+ 
+}
+
+self.onmessage = function (event) {
+  const content = event.data;
+  csvProcess({ content });
+};
+
+
+function validateLine(line, lineNumber) {
+  if (line.length !== 6) {
+    errors.push({ line: lineNumber, msg: "Invalid number of columns" });
+  }
+
+  const date = new Date(line[2]);
+  if (isNaN(date.getTime())) {
+    errors.push({ line: lineNumber, msg: "Invalid date" });
+  }
+}
